d4034d18d51bf02e8a0229745d7895238c5ada70
namaba <kensuke.namba@msetsu.com>
2020-06-01 22:43:54 +0900

    feat: 本番用メールアドレス追加&twitter認証時メール送信しない

--- app/models/user.rb
+++ app/models/user.rb
@@ -41,20 +41,18 @@ class User < ApplicationRecord
          :confirmable, :lockable, :timeoutable, :trackable, :omniauthable, omniauth_providers:[:twitter]
 
   def self.from_omniauth(auth)
-    find_or_create_by(provider: auth["provider"], uid: auth["uid"]) do |user|
+    find_or_initialize_by(provider: auth["provider"], uid: auth["uid"]) do |user|
       user.provider = auth["provider"]
       user.uid = auth["uid"]
       user.username = auth["info"]["nickname"]
+      user.password = Devise.friendly_token[0, 20]
+      user.email = User.dumy_email(auth)
     end
   end
 
-  def self.new_with_session(params, session)
-    if session["devise.user_attributes"]
-      new(session["devise.user_attributes"]) do |user|
-        user.attributes = params
-      end
-    else
-      super
-    end
+  private
+
+  def self.dumy_email(auth)
+    "#{auth.uid}-#{auth.provider}@example.com"
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,6 +60,16 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "cosplayers_production"
 
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => 'smtp.gmail.com',
+    :port => 587,
+    :user_name => ENV['GMAIL'],
+    :password => ENV['GMAIL_PASSWORD'],
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
