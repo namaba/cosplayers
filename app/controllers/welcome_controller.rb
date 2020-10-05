767d2f3d7da15d234334a0cc6397ce4d799784d6
namaba <kensuke.namba@msetsu.com>
2020-10-05 23:36:04 +0900

    ui: top&cosplayer&photoのindexページのスタイル変更

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-    @creaters = Creater.limit(12)
-    @photos = Work.photo.includes(:user).limit(12)
+    @creaters = Creater.limit(8)
+    @photos = Work.photo.includes(:user).limit(8)
   end
 end
