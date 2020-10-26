6441e17ee95cf1f011712ebe8bc3c72730c4cdeb
namaba <kensuke.namba@msetsu.com>
2020-10-27 00:09:14 +0900

    feat: 利用規約追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'welcome#index'
   get '/privacy' => 'welcome#privacy'
+  get '/terms' => 'welcome#terms'
+
   # 作品の新規登録、編集、削除用
   namespace :creater do
     resources :works
